0395380e75598a9519cdeb6897d83bc81da0677c
mintusikder <mintusikder15@gmail.com>
2024-02-24 14:24:20 +0600

    added comment

--- src/assets/components/Modal.jsx
+++ src/assets/components/Modal.jsx
@@ -1,34 +1,47 @@
 import React from "react";
 
 const Modal = (props) => {
-  // console.log(props.singleData)
+  // Destructure props
   const { image_link, description, features, integrations } = props.singleData;
-  // console.log(integrations);
+
   return (
     <div>
+      {/* Modal trigger checkbox */}
       <input type="checkbox" id="my_modal_6" className="modal-toggle" />
+
+      {/* Modal container */}
       <div className="modal" role="dialog">
         <div className="modal-box w-11/12 max-w-5xl">
-          <div className="card lg:card-side bg-base-100 ">
+          <div className="card lg:card-side bg-base-100">
             <div className="card-body">
+              {/* Modal title */}
               <h2 className="card-title">{description}</h2>
 
               <div className="flex justify-between">
                 <div>
-                  <h1 className="text-xl font-bold">Feaures</h1>
-                  {Object.values(features || {}).map((value) => (
-                    <li>{value.feature_name}</li>
-                  ))}
+                  {/* Features */}
+                  <h1 className="text-xl font-bold">Features</h1>
+                  <ul>
+                    {Object.values(features || {}).map((value, index) => (
+                      <li key={index}>{value.feature_name}</li>
+                    ))}
+                  </ul>
                 </div>
+
                 <div>
+                  {/* Integrations */}
                   <h1 className="text-xl font-bold">Integrations</h1>
-                  {integrations.map((inte) => (
-                    <li>{inte}</li>
-                  ))}
+                  <ul>
+                    {integrations &&
+                      integrations.map((inte, index) => (
+                        <li key={index}>{inte}</li>
+                      ))}
+                  </ul>
                 </div>
               </div>
             </div>
-            <figure>
+            {/* Modal image */}
+            <figure className="w-full">
               <img
                 className="w-full h-64"
                 src={image_link && image_link[0]}
@@ -36,6 +49,7 @@ const Modal = (props) => {
               />
             </figure>
           </div>
+          {/* Modal close button */}
           <div className="modal-action">
             <label htmlFor="my_modal_6" className="btn">
               Close!
